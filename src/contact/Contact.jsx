a7277a1a1fd44852c8687aac2000ee5e7a9d3cb6
Steve B <sbogucki@mail.usf.edu>
2021-07-09 09:02:53 -0700

    wiring up contact form

--- src/contact/Contact.jsx
+++ src/contact/Contact.jsx
@@ -1,18 +1,20 @@
 import { useState } from 'react';
 import Captcha from './Captcha';
 import './contact.css';
+import http from '../utility/http';
 
 const Contact = () => {
 	const [formData, setFormData] = useState({
-		name: '',
-		email: '',
-		subject: '',
-		message: '',
-	}); 
+		subject: 'This is a test from development - local host',
+		content: 'Hi. I hope this works.',
+		customerName: 'Steve B',
+		customerEmail: 'test@test.com',
+		dateStamp: '09 July2021 08:57',
+	});
 	const [isCaptchaValid, setIsCaptchaValid] = useState(false);
 
 	const handleSubmit = (e) => {
-        e.preventDefault();
+		e.preventDefault();
 		console.log(isCaptchaValid);
 	};
 
@@ -23,16 +25,52 @@ const Contact = () => {
 		});
 	};
 
+	const handleClick = () => {
+		http(
+			'https://djbogoodskiapi.azurewebsites.net/api/SendGrid1?code=' + process.env.REACT_APP_SENDGRID_KEY,
+			'POST',
+			{
+				subject: formData.subject,
+				content: formData.content,
+				customerName: formData.customerName,
+				customerEmail: formData.customerEmail,
+			}
+		).then((data) => console.log(data));
+	};
+
 	return (
 		<div className="contactContainer">
 			<form onSubmit={handleSubmit} className="centerAlign">
 				<label className="centerAlign">
 					Name:
-					<input type="text" value={formData.name} name="name" onChange={(e) => handleChange(e)} />
+					<input
+						type="text"
+						value={formData.customerName}
+						name="customerName"
+						onChange={(e) => handleChange(e)}
+					/>
+				</label>
+				<label className="centerAlign">
+					Email:
+					<input
+						type="email"
+						value={formData.customerEmail}
+						name="customerEmail"
+						onChange={(e) => handleChange(e)}
+					/>
+				</label>
+				<label className="centerAlign">
+					Subject:
+					<input type="text" value={formData.subject} name="subject" onChange={(e) => handleChange(e)} />
+				</label>
+				<label className="centerAlign">
+					Message:
+					<input type="text" value={formData.content} name="content" onChange={(e) => handleChange(e)} />
 				</label>
 				<Captcha isCaptchaValid={isCaptchaValid} setIsCaptchaValid={setIsCaptchaValid} />
 				<input type="submit" value="Submit" />
 			</form>
+			<button onClick={() => handleClick()}>TEST</button>
 		</div>
 	);
 };
--- src/highlight/Highlight.jsx
+++ src/highlight/Highlight.jsx
@@ -1,6 +1,5 @@
 import highlightData from './cheatcodevol8.json';
 import './highlight.css';
-import http from '../utility/http';
 
 const Highlight = () => {
 	const tracks = highlightData.tracks.map((track) => (
@@ -9,23 +8,19 @@ const Highlight = () => {
 		</li>
 	));
 
-	const handleClick = () => {
-		http('https://djbogoodskiapi.azurewebsites.net/api/SendGrid1?code=' + process.env.REACT_APP_SENDGRID_KEY, 'POST', {
-			"subject": "This is from development.",
-			"content": "Proin hendrerit aliquet neque, sit amet tempor urna ultrices sit amet. Vivamus scelerisque eros dui, et sagittis massa rhoncus ac. Nulla maximus risus rhoncus vestibulum lobortis. Fusce cursus justo quis diam ullamcorper porttitor. Donec non porta augue, nec vestibulum libero. Vivamus finibus, lectus et pretium tempus, arcu augue lacinia ex, cursus vestibulum dolor velit vel mauris. Integer rhoncus lacinia sollicitudin. Aenean auctor sapien vel euismod molestie. Morbi velit velit, faucibus in ante id, pretium molestie odio. Sed ultricies eros varius arcu euismod mattis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Mauris quis justo sit amet est consectetur aliquet. Quisque bibendum pellentesque turpis, sed porta diam bibendum non. Nullam eget rhoncus diam.",
-			"customerName": "Steve B.",
-			"customerEmail": "email@email.com"
-		  } ).then(data => console.log(data))
-	}
 	return (
 		<div className="highlightContainer">
 			<h1>{highlightData.title}</h1>
-			<img src={highlightData.image} style={{ width: 250, height: '70%', borderRadius: 8 }} className="highlightImage" alt="Cheat Code volume 8"/>
+			<img
+				src={highlightData.image}
+				style={{ width: 250, height: '70%', borderRadius: 8 }}
+				className="highlightImage"
+				alt="Cheat Code volume 8"
+			/>
 			<p>
 				<a href={highlightData.link}>Listen Now!</a>
 			</p>
 			<ul>{tracks}</ul>
-			<button onClick={() => handleClick()}>TEST</button>
 		</div>
 	);
 };
